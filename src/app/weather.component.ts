a39f37824b69650adea973bfe610a084e3c8f489
vigneshramamurthy1 <vigneshramamurthy1991@gmail.com>
2020-08-19 01:24:48 +0530

    WeatherApp

--- src/app/weather.component.ts
+++ src/app/weather.component.ts
@@ -8,6 +8,7 @@ import { WeatherService } from './weather.service';
 })
 export class WeatherComponent  implements OnInit{
   data : any = [];
+  ApiKey = "fb82e02617662394f4f32428ea3c45db";
   constructor(private weatherService: WeatherService){
 
   }
@@ -64,13 +65,13 @@ export class WeatherComponent  implements OnInit{
   weatherStatus7 = "";
   weatherStatus8 = "";
   weatherStatus9 = "";
-  temp1 = "";
-  temp2 = "";
-  temp3 = "";
-  temp4 = "";
-  temp5 = "";
-  temp6 = "";
-  temp7 = "";
+  temp1 = 0;
+  temp2 = 0;
+  temp3 = 0;
+  temp4 = 0;
+  temp5 = 0;
+  temp6 = 0;
+  temp7 = ;
   temp8 = "";
   temp9 = "";
   humudity1 ="";
@@ -115,7 +116,10 @@ export class WeatherComponent  implements OnInit{
     alert(this.valueInput1)
     var data = this.data.filter(f=> f["name"] === this.valueInput1);
     console.log(data)
-    if(data.length > 1 ){
+    if(data && data[0] ){
+	this.showError1 ="";
+	}
+	else{
     this.showError1 = "City is not Available Or Wrong City Name";
     }
     }
@@ -123,56 +127,80 @@ export class WeatherComponent  implements OnInit{
     this.valueInput2 = value; 
     var data = this.data.filter(f=> f["name"] === this.valueInput2);
     console.log(data)
-    if(data.length > 1 ){
-    this.showError1 = "City is not Available Or Wrong City Name";
+    if(data && data[0] ){
+		this.showError2 ="";
+    }
+    else{
+    this.showError2 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput3(value: string) { 
     this.valueInput3 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data =  this.data.filter(f=> f["name"] === this.valueInput3);
+    if(data && data[0] ){
+		this.showError3 ="";
+	}
+    else{
     this.showError3 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput4(value: string) { 
     this.valueInput4 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data =  this.data.filter(f=> f["name"] === this.valueInput4);
+    if(data && data[0] ){
+		this.showError4 ="";
+	}
+    else{
     this.showError4 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput5(value: string) { 
     this.valueInput5 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data = this.data.filter(f=> f["name"] === this.valueInput5);
+    if(data && data[0] ){
+		this.showError5 ="";
+	}
+    else{
     this.showError5 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput6(value: string) { 
     this.valueInput6 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data = this.data.filter(f=> f["name"] === this.valueInput6);
+    if(data && data[0] ){
+		this.showError6 ="";
+	}
+    else{
     this.showError6 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput7(value: string) { 
     this.valueInput7 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data = this.data.filter(f=> f["name"] === this.valueInput7);
+    if(data && data[0]){
+		this.showError7 ="";
+	}
+    else{
     this.showError7 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput8(value: string) { 
     this.valueInput8 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data =  this.data.filter(f=> f["name"] === this.valueInput8);
+    if(data && data[0] ){
+		this.showError8 ="";
+	}
+    else{
     this.showError8 = "City is not Available Or Wrong City Name";
     }
     }
     updateInput9(value: string) { 
     this.valueInput9 = value; 
-    this.data.filter(f=> f["name"] === this.valueInput1);
-    if(this.data.length > 1 ){
+    var data =  this.data.filter(f=> f["name"] === this.valueInput9);
+    if(data && data[0] ){
+		this.showError9 ="";
+	}
+    else{
     this.showError9 = "City is not Available Or Wrong City Name";
     }
     }
@@ -215,6 +243,84 @@ export class WeatherComponent  implements OnInit{
     }
 
 
+    getOpenWetherApi(obj, event){
+		let id = obj.id;
+		this.weatherService.getWeather(obj, this.ApiKey).subscribe(data =>{
+		var obj = data;
+		if(event ==1){
+		this.citi1 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp1 = temp - 273.15;
+		this.weatherStatus1 = obj["weather"]["description"];
+		this.humudity1 = obj["main"]["humidity"];
+		this.windSpeed1 = obj["wind"]["speed"];
+	    }
+		if(event == 2){
+		this.citi2 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp2 = temp - 273.15;
+		this.weatherStatus2 = obj["weather"]["description"];
+		this.humudity2 = obj["main"]["humidity"];
+		this.windSpeed2 = obj["wind"]["speed"];
+	    } 
+		if(event == 3){
+		this.citi3 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp3 = temp - 273.15;
+		this.weatherStatus3 = obj["weather"]["description"];
+		this.humudity3 = obj["main"]["humidity"];
+		this.windSpeed3 = obj["wind"]["speed"];
+	    } 
+		if(event == 4){
+		this.citi4 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp4 = temp - 273.15;
+		this.weatherStatus4 = obj["weather"]["description"];
+		this.humudity4 = obj["main"]["humidity"];
+		this.windSpeed4 = obj["wind"]["speed"];
+	    }
+		if(event == 5){
+		this.citi5 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp5 = temp - 273.15;
+		this.weatherStatus5 = obj["weather"]["description"];
+		this.humudity5 = obj["main"]["humidity"];
+		this.windSpeed5 = obj["wind"]["speed"];
+	    }
+		if(event == 6){
+		this.citi6 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp6 = temp - 273.15;
+		this.weatherStatus6 = obj["weather"]["description"];
+		this.humudity6 = obj["main"]["humidity"];
+		this.windSpeed6 = obj["wind"]["speed"];
+	    }
+		if(event == 7){
+		this.citi7 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp7 = temp - 273.15;
+		this.weatherStatus7 = obj["weather"]["description"];
+		this.humudity7 = obj["main"]["humidity"];
+		this.windSpeed7 = obj["wind"]["speed"];
+	    }
+		if(event == 8){
+		this.citi8 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp8 = temp - 273.15;
+		this.weatherStatus8 = obj["weather"]["description"];
+		this.humudity8 = obj["main"]["humidity"];
+		this.windSpeed8 = obj["wind"]["speed"];
+	    }
+		if(event == 9){
+		this.citi9 = obj["name"];
+		var temp = obj["main"]["temp"];
+		this.temp9 = temp - 273.15;
+		this.weatherStatus9 = obj["weather"]["description"];
+		this.humudity9 = obj["main"]["humidity"];
+		this.windSpeed9 = obj["wind"]["speed"];
+	    }
+		});
+    }
 
 
 
